0eb94b9c7693d930eaeb6221514bd2d11f006064
JAVB2077 <joe.tecnm@gmail.com>
2025-03-05 10:50:11 -0600

    Creacion del proyecto

---
+++ src/test/java/com/proyect/ptics/PTicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyect.ptics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PTicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
